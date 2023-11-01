78fa3240793b84160f00de6cf70fd49f166037c3
Anh Vũ <vn.vna@outlook.com>
2023-11-01 14:21:08 +0700

    Change frontend to React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Button } from 'react-bootstrap'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -17,14 +18,12 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <Button size='sm' color='secondary' onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
